d42857f94e2054de5375b02a5a2dcdee8e8ebf60
septiadiyudhi-cmd <septiadiyudhi@gmail.com>
2026-09-05 01:02:24 +0700

    Update dashboard_cb_streamlit.py

--- dashboard_cb_streamlit.py
+++ dashboard_cb_streamlit.py
@@ -192,7 +192,7 @@ else:
 # =============================================================
 # TAMPILAN DASHBOARD GLOBAL (LOGO & HEADER)
 # =============================================================
-col_logo, col_judul = st.columns([1.5, 14])
+col_logo, col_judul = st.columns([1.5, 15])
 with col_logo: 
     if os.path.exists("logo_bmkg_2.png"):
         st.image("logo_bmkg_2.png", width=130)
