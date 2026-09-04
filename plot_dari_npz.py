84a239565770878e88981eff782d78b4ca32b862
septiadiyudhi-cmd <septiadiyudhi@gmail.com>
2026-09-05 04:28:53 +0700

    Update plot_dari_npz.py

--- plot_dari_npz.py
+++ plot_dari_npz.py
@@ -47,7 +47,7 @@ POLA_TIMESTAMP = re.compile(r"(\d{12})\.nc$", re.IGNORECASE)
 
 INFO_RADAR = {
     "SURABAYA": {"lat": -7.230, "lon": 113.030},
-    "DENPASAR": {"lat": -8.748, "lon": 115.767}, # Posisi bujur Denpasar sudah digeser ke Timur
+    "DENPASAR": {"lat": -8.748, "lon": 115.177}, # Posisi bujur Denpasar sudah digeser ke Timur
 }
 RADIUS_RADAR_KM = 250.0
 
