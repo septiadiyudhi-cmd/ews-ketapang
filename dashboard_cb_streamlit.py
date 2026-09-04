2321e672f4e975c7078834a819f8093492a062e5
septiadiyudhi-cmd <septiadiyudhi@gmail.com>
2026-09-05 00:58:36 +0700

    Update dashboard_cb_streamlit.py

--- dashboard_cb_streamlit.py
+++ dashboard_cb_streamlit.py
@@ -80,7 +80,7 @@ st.markdown(
     .stApp { background-color: #0e1117; }
     /* Memperbesar ukuran teks pada Menu Tab */
     .stTabs [data-baseweb="tab-list"] button [data-testid="stMarkdownContainer"] p {
-        font-size: 42px;  /* Angka ini diperbesar */
+        font-size: 50px;  /* Angka ini diperbesar */
         font-weight: bold; /* Dibuat lebih tebal */
     }
     </style>
